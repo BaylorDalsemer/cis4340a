838e0d08f06cd2ffb6bee1ace746e0c8e9e195ea
BaylorDalsemer <162058762+BaylorDalsemer@users.noreply.github.com>
2024-03-03 16:28:23 -0500

    Create R03_NUM03_J.java

---
+++ R03_NUM03_J.java
@@ -0,0 +1,9 @@
+// noncompliant code example 
+public static int getInteger(DataInputStream is) throws IOException {
+  return is.readInt(); 
+}
+
+// compliant code example
+public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+}
