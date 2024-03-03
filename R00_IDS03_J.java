2004bb2dc0865cc58225818cbb35a1d1e5b2714b
BaylorDalsemer <162058762+BaylorDalsemer@users.noreply.github.com>
2024-03-03 16:23:41 -0500

    Create R00_IDS03_J.java

---
+++ R00_IDS03_J.java
@@ -0,0 +1,18 @@
+//Non Compliant Code Example
+if (loginSuccessful) {
+  logger.severe("User login succeeded for: " + username);
+} else {
+  logger.severe("User login failed for: " + username);
+}
+
+// compliant code example
+if (loginSuccessful) {
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
+} else {
+  logger.severe("User login failed for: " + sanitizeUser(username));
+}
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
+}
+
